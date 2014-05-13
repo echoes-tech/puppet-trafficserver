f19edcbc3a0e723f50198d8f7262fe3637e08327
Titotix <titotix@free.fr>
2014-05-13 16:01:16 +0000

    Add script to templates

---
+++ manifests/firewall.pp
@@ -0,0 +1,15 @@
+class trafficserver::firewall
+(
+  $todest  = undef,
+)
+{
+firewall { '010 Foreward http to ats':
+  dport   => [ 80 ],
+  proto   => 'tcp',
+  jump    => 'DNAT',
+  chain   => 'PREROUTING',
+  table   => 'nat',
+  iniface => 'eth0',
+  todest  => "${todest}:80",
+  }
+}
---
+++ manifests/healthcheck.pp
@@ -0,0 +1,38 @@
+class trafficserver::healthcheck
+(
+  $balancer_map     = balancer_map,
+  $balancer_algo    = balancer_algo,
+  $balancer_backend = balancer_backend,
+) inherits trafficserver
+{
+  file { "$bindir/traffic_healthcheck.sh":
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0754',
+    content => template('trafficserver/healthcheck.sh.erb'),
+  }->
+
+  exec { 'trafficserver-healthcheck':
+    command     => "$bindir/traffic_healthcheck.sh", 
+    #refreshonly => true,
+  }->
+
+  Exec['trafficserver-config-reload']
+
+  file { "$bindir/traffic_healthcheck_daemon.sh":
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0754',
+    content => template('trafficserver/healthcheck_daemon.sh.erb'),
+  }
+
+  #exec { "$bindir/traffic_healthcheck_daemon.sh &":
+  #path    => [
+  #  '/usr/local/bin',
+  #  '/usr/bin',
+  #  '/bin'],
+  #unless  => 'ps aux | grep \'[t]raffic_healthcheck_daemon.sh\' > /dev/null',
+  #}
+}
